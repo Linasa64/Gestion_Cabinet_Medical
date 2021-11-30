50e0d2face76610c874bea0ec4ca1948ffde7608
Obain <86167470+Obain@users.noreply.github.com>
2021-11-30 11:55:40 +0100

    SaisieMedecin.php

--- SaisieMedecin.php
+++ SaisieMedecin.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 
 <head>
@@ -9,7 +9,7 @@
 <body>
 
     <h1>Saisie d'un nouveau Médecin</h1>
-    <form action="ajoutMedecin.php" method="post">
+    <form action="SaisieMedecin.php" method="post">
         Civilité : <input type = text name="Civilite" /><br>
         Nom : <input type="text" name="Nom"><br>
         Prenom : <input type="text" name="Prenom" /><br>
@@ -17,6 +17,28 @@
         <input type="submit" value="Ajout" name="ajout">
         <input type="reset" value="Reset" name="reset">
     </form>
+
+    <?php
+
+        include 'resrc/Connect.php';
+        
+        //La requête
+
+        $civilite = $_POST['Civilite'];
+        $nom = $_POST['Nom'];
+        $prenom = $_POST['Prenom'];
+
+        $requete = 'INSERT INTO medecin(Civilite, Nom, Prenom) 
+                    VALUES("'.$civilite.'", "'.$nom.'", "'.$prenom.'")';
+        if (!empty($civilite) && !empty($nom) && !empty($prenom)){
+            $result = mysqli_query($link, $requete);
+            header('Location: index.php');
+        }
+        else{
+            print("oh nooooo");
+        }
+    
+    ?>
 
 </body>
 
